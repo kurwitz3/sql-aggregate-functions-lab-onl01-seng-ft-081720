13b41ab5f6c2b1c24809d67c24e8ec81dacdf100
Richard Kurwitz <kurwitz3@aol.com>
2020-09-14 20:52:18 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-   "SELECT SUM(TARDIES) FROM * students;"
+   "SELECT SUM(TARDIES) FROM  students;"
 end
 
 def average_gpa_for_9th_grade
